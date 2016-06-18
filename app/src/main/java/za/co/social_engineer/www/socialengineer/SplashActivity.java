1215a2a51277e1f4307070520d219df1dfbc1f84
Marcel Teixeira <marceltex@gmail.com>
2016-06-19 00:46:30 +0200

    Added code to hide action bar on splash activity

--- app/src/main/java/za/co/social_engineer/www/socialengineer/SplashActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/SplashActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 /**
  * Activity to display the splash screen on app startup.
@@ -24,6 +25,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getActionBar().hide();
+
         setContentView(R.layout.activity_splash);
 
         if (savedInstanceState != null) {
