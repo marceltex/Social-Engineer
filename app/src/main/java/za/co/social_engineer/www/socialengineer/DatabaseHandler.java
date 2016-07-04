f72bb747aba468ec4b2129b7d848f9c6bf8860f1
Marcel Teixeira <marceltex@gmail.com>
2016-07-04 17:57:38 +0200

    Add constructor and simple 'getAll rows from table' method to database handler

--- app/src/main/java/za/co/social_engineer/www/socialengineer/DatabaseHandler.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/DatabaseHandler.java
@@ -1,5 +1,10 @@
 package za.co.social_engineer.www.socialengineer;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 /**
  * Class used to manage database CRUD
  *
@@ -7,5 +12,35 @@ package za.co.social_engineer.www.socialengineer;
  */
 public class DatabaseHandler {
 
+    private String databaseUrl;
+    private String databasePort;
+    private String databaseName;
+    private String username;
+    private String password;
 
+    public DatabaseHandler(String databaseUrl, String databasePort, String databaseName, String username, String password) {
+        this.databaseUrl = databaseUrl;
+        this.databasePort = databasePort;
+        this.databaseName = databaseName;
+        this.username = username;
+        this.password = password;
+    }
+
+    public ResultSet getAll() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection conn = DriverManager.getConnection("jdbc:mysql://" + databaseUrl + ":" +
+                    databasePort + "/" + databaseName, username, password);
+
+            Statement stmnt = conn.createStatement();
+
+            ResultSet rs = stmnt.executeQuery("SELECT * FROM questions");
+
+            return rs;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
