7f6c84d273ce6873725b9470d3da76f7babc3d29
Marcel Teixeira <marceltex@gmail.com>
2016-07-04 09:03:01 +0200

    Add mysql-connector java binary to project

--- app/src/main/java/za/co/social_engineer/www/socialengineer/DatabaseHandler.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/DatabaseHandler.java
@@ -1,7 +1,11 @@
 package za.co.social_engineer.www.socialengineer;
 
 /**
- * Created by Marcel on 2016/07/03.
+ * Class used to manage database CRUD
+ *
+ * Created by Marcel Teixeira on 2016/07/03.
  */
 public class DatabaseHandler {
+
+
 }
