92aaa894025b16daf8c3ce2a4ea248ac1a1185cb
Marcel Teixeira <marceltex@gmail.com>
2016-07-05 00:43:22 +0200

    Fix build bug

--- app/src/main/java/za/co/social_engineer/www/socialengineer/SEADMActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/SEADMActivity.java
@@ -1,19 +1,16 @@
 package za.co.social_engineer.www.socialengineer;
 
-import android.os.Bundle;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-
-/**
- * Activity to guide the user through the SEADMv2 to detect whether the user is the victim of a social
- * engineering attack or not.
- *
- * Created by Marcel Teixeira on 2016/06/19
- */
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SEADMActivity extends AppCompatActivity {
+
+    private Button yesBtn;
+    private Button noBtn;
+
+    private TextView questionTxtView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,36 +19,22 @@ public class SEADMActivity extends AppCompatActivity {
 
         SplashActivity.splash.finish(); // Finish Splash Activity, so that if user presses back button, it is not displayed
 
-        DatabaseHandler databaseHandler = new DatabaseHandler("moutonf.co.za", "3306", "SEPTT",
-                "septt", "toor");
+        yesBtn = (Button) findViewById(R.id.button_yes);
+        noBtn = (Button) findViewById(R.id.button_no);
 
+        questionTxtView = (TextView) findViewById(R.id.text_view_question);
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_seadm, menu);
-
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), "Todo...", Snackbar.LENGTH_LONG);
-
-        switch (id) {
-            case R.id.menu_item_help:
-            case R.id.menu_item_about:
-                snackbar.show();
-                break;
-        }
-
-        return super.onOptionsItemSelected(item);
+        //        DatabaseHandler databaseHandler = new DatabaseHandler("moutonf.co.za", "3306", "SEPTT",
+//                "septt", "toor");
+//
+//        try {
+//            ResultSet rs = databaseHandler.getAll();
+//
+//            rs.next();
+//
+//            questionTxtView.setText(rs.getString(3));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
     }
 }
