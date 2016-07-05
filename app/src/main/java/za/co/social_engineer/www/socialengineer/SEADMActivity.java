6c3196302bbc81e6a24470e1add66a0324165fe7
Marcel Teixeira <marceltex@gmail.com>
2016-07-05 09:17:30 +0200

    Add OnClickListeners for yes and no buttons. These still need to be implemented

--- app/src/main/java/za/co/social_engineer/www/socialengineer/SEADMActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/SEADMActivity.java
@@ -1,40 +1,63 @@
 package za.co.social_engineer.www.socialengineer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.sql.ResultSet;
+
 public class SEADMActivity extends AppCompatActivity {
 
-    private Button yesBtn;
-    private Button noBtn;
+    private Button yesButton;
+    private Button noButton;
 
-    private TextView questionTxtView;
+    private TextView questionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seadm);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
         SplashActivity.splash.finish(); // Finish Splash Activity, so that if user presses back button, it is not displayed
 
-        yesBtn = (Button) findViewById(R.id.button_yes);
-        noBtn = (Button) findViewById(R.id.button_no);
+        yesButton = (Button) findViewById(R.id.button_yes);
+        noButton = (Button) findViewById(R.id.button_no);
 
-        questionTxtView = (TextView) findViewById(R.id.text_view_question);
+        questionTextView = (TextView) findViewById(R.id.text_view_question);
 
-        //        DatabaseHandler databaseHandler = new DatabaseHandler("moutonf.co.za", "3306", "SEPTT",
-//                "septt", "toor");
-//
-//        try {
-//            ResultSet rs = databaseHandler.getAll();
-//
-//            rs.next();
-//
-//            questionTxtView.setText(rs.getString(3));
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        DatabaseHandler databaseHandler = new DatabaseHandler("moutonf.co.za", "3306", "SEPTT",
+                "septt", "toor");
+
+        // Set text of questionTextView to the string of the first question of the SEADM
+        try {
+            ResultSet rs = databaseHandler.getFirstQuestion();
+
+            rs.next();
+
+            questionTextView.setText(rs.getString(3));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        yesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        noButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
