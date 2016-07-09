3a8a1b876339354b7d2239b116626300aa65845d
Marcel Teixeira <marceltex@gmail.com>
2016-07-09 21:43:43 +0200

    Add Question.java

---
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/Model/Question.java
@@ -0,0 +1,9 @@
+package za.co.social_engineer.www.socialengineer.Model;
+
+/**
+ * Question class used to create question objects of each record in the questions database table.
+ *
+ * Created by Marcel Teixeira on 2016/07/09.
+ */
+public class Question {
+}
