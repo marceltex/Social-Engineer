18746f9c80de20999f731f80cd9c7d0fcbeb81c7
Marcel Teixeira <marceltex@gmail.com>
2016-07-10 00:45:08 +0200

    Start implementing BaseApiClient.java

--- app/src/main/java/za/co/social_engineer/www/socialengineer/api/BaseApiClient.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/api/BaseApiClient.java
@@ -1,7 +1,21 @@
 package za.co.social_engineer.www.socialengineer.api;
 
+import android.content.Context;
+
+import java.net.CookieManager;
+
 /**
  * Created by Marcel Teixeira on 2016/07/10.
  */
-public abstract class BaseApiClient {
+public abstract class BaseApiClient<T> {
+
+    private T server;
+    private Context context;
+    private Class<T> serverClass;
+    private CookieManager mCookieManager;
+
+    public BaseApiClient(Context context, Class<T> serverClass) {
+        this.context = context;
+        this.serverClass = serverClass;
+    }
 }
