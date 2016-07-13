6c74af1c13fa67aa76accdcd4abae8920cd21c48
Marcel Teixeira <marceltex@gmail.com>
2016-07-13 22:55:14 +0200

    Add SQL files which contain data to be stored in various database tables

---
+++ app/src/main/assets/State.sql
@@ -0,0 +1,10 @@
+INSERT INTO State (id, name) VALUES
+(1, 'Start State'),
+(2, 'State Y1'),
+(3, 'State Blue'),
+(4, 'State Y2'),
+(5, 'State G1'),
+(6, 'State R'),
+(7, 'State G2'),
+(100, 'Fail'),
+(200, 'Sucess');
