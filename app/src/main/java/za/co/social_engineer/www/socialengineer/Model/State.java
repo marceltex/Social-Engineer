bc77355d9f699be6e8054fd6f0afa85a7bad9619
Marcel Teixeira <marceltex@gmail.com>
2016-07-13 02:19:38 +0200

    Make State.java parcelable

--- app/src/main/java/za/co/social_engineer/www/socialengineer/Model/State.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/Model/State.java
@@ -1,19 +1,17 @@
 package za.co.social_engineer.www.socialengineer.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * State class used to create state objects of each record in the State database table.
  *
  * Created by Marcel Teixeira on 2016/07/09.
  */
-public class State {
+public class State implements Parcelable {
 
     private String id;
     private String name;
-
-    public State(String id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
     public String getId() {
         return id;
@@ -30,4 +28,34 @@ public class State {
     public void setName(String name) {
         this.name = name;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.id);
+        dest.writeString(this.name);
+    }
+
+    public State() {
+
+    }
+
+    protected State(Parcel in) {
+        this.id = in.readString();
+        this.name = in.readString();
+    }
+
+    public static final Parcelable.Creator<State> CREATOR = new Parcelable.Creator<State>() {
+        public State createFromParcel(Parcel source) {
+            return new State(source);
+        }
+
+        public State[] newArray(int size) {
+            return new State[size];
+        }
+    };
 }
