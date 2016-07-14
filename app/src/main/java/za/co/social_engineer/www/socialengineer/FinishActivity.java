8c01b00592630e0e60e28c1d28ac37c2213e8866
Marcel Teixeira <marceltex@gmail.com>
2016-07-14 16:02:49 +0200

    Add code to display text of final state and set colour of text appropriately

--- app/src/main/java/za/co/social_engineer/www/socialengineer/FinishActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/FinishActivity.java
@@ -1,13 +1,55 @@
 package za.co.social_engineer.www.socialengineer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+import za.co.social_engineer.www.socialengineer.model.Question;
 
 public class FinishActivity extends AppCompatActivity {
+
+    private Button restartButton;
+
+    private TextView resultTextView;
+
+    private Question finalQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish);
+
+        restartButton = (Button) findViewById(R.id.button_restart);
+
+        resultTextView = (TextView) findViewById(R.id.text_view_result);
+
+        Vibrator vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        if (savedInstanceState != null) {
+            finalQuestion = savedInstanceState.getParcelable(SEADMActivity.FINAL_QUESTION);
+        } else {
+            Intent intent = getIntent();
+            finalQuestion = intent.getParcelableExtra(SEADMActivity.FINAL_QUESTION);
+        }
+
+        if (finalQuestion.getQuestionSet() == 100) {
+            resultTextView.setTextColor(Color.RED);
+            vibrator.vibrate(500);
+        } else {
+            resultTextView.setTextColor(Color.GREEN);
+        }
+
+        resultTextView.setText(finalQuestion.getQuestion());
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putParcelable(SEADMActivity.FINAL_QUESTION, finalQuestion);
+
+        super.onSaveInstanceState(savedInstanceState);
     }
 }
