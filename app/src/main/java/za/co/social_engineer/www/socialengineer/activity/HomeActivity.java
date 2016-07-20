cfc6b41528908d39d576a8e091d315d98e1abdda
Marcel Teixeira <marceltex@gmail.com>
2016-07-20 16:26:53 +0200

    Add methods to handle each button click
    When questions button is clicked, SEADM Activity is started. All other
    button clicks currently display a 'Todo' toast notification.

--- app/src/main/java/za/co/social_engineer/www/socialengineer/activity/HomeActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/activity/HomeActivity.java
@@ -1,15 +1,46 @@
 package za.co.social_engineer.www.socialengineer.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import za.co.social_engineer.www.socialengineer.R;
+import za.co.social_engineer.www.socialengineer.model.Question;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private Question firstQuestion;
+
+    private Toast todoToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        Intent intent = getIntent();
+        firstQuestion = intent.getParcelableExtra(SplashActivity.FIRST_QUESTION);
+
+        todoToast = Toast.makeText(this, "Todo", Toast.LENGTH_LONG);
+    }
+
+    public void questionsButtonClicked(View view) {
+        Intent intent = new Intent(HomeActivity.this, SEADMActivity.class);
+        intent.putExtra(SplashActivity.FIRST_QUESTION, firstQuestion);
+        startActivity(intent);
+    }
+
+    public void pictureOfModelButtonClicked(View view) {
+        todoToast.show();
+    }
+
+    public void trainingButtonClicked(View view) {
+        todoToast.show();
+    }
+
+    public void aboutButtonClicked(View view) {
+        todoToast.show();
     }
 }
