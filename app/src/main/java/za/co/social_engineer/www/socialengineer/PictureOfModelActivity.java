fd263b97ad0b451dc42a635f2d9619a4e725c448
Marcel Teixeira <marceltex@gmail.com>
2016-08-07 00:56:13 +0200

    Add Picture of Model Activity, which will display a visual representation of the current version of the SEADM

---
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/PictureOfModelActivity.java
@@ -0,0 +1,13 @@
+package za.co.social_engineer.www.socialengineer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PictureOfModelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_of_model);
+    }
+}
