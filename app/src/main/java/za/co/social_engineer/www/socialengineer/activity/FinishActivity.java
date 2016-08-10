df86c85b834762c235d493ffd8ccbbf761713b3b
Marcel Teixeira <marceltex@gmail.com>
2016-08-10 14:05:50 +0200

    Add state buttons to FinishActivity.java

--- app/src/main/java/za/co/social_engineer/www/socialengineer/activity/FinishActivity.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/activity/FinishActivity.java
@@ -6,6 +6,7 @@ import android.os.Vibrator;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import za.co.social_engineer.www.socialengineer.R;
@@ -15,6 +16,14 @@ import za.co.social_engineer.www.socialengineer.model.Question;
 public class FinishActivity extends AppCompatActivity {
 
     private TextView resultTextView;
+
+    private Button state1Button;
+    private Button state2Button;
+    private Button state3Button;
+    private Button state4Button;
+    private Button state5Button;
+    private Button state6Button;
+    private Button finalStateButton;
 
     private Question finalQuestion;
 
@@ -26,6 +35,14 @@ public class FinishActivity extends AppCompatActivity {
         setContentView(R.layout.activity_finish);
 
         resultTextView = (TextView) findViewById(R.id.text_view_result);
+
+        state1Button = (Button) findViewById(R.id.button_state_1);
+        state2Button = (Button) findViewById(R.id.button_state_2);
+        state3Button = (Button) findViewById(R.id.button_state_3);
+        state4Button = (Button) findViewById(R.id.button_state_4);
+        state5Button = (Button) findViewById(R.id.button_state_5);
+        state6Button = (Button) findViewById(R.id.button_state_6);
+        finalStateButton = (Button) findViewById(R.id.button_final_state);
 
         Intent intent = getIntent();
         finalQuestion = intent.getParcelableExtra(SEADMActivity.FINAL_QUESTION);
