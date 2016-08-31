a23ebd3ce56f73dc9f5f1ee46a8739215dc04e5f
Marcel Teixeira <marceltex@gmail.com>
2016-08-31 23:26:48 +0200

    Remove code to handle circular dependency from getNextQuestion method, since circular dependencies no longer exist in the SEADM

--- app/src/main/java/za/co/social_engineer/www/socialengineer/api/DatabaseHandler.java
+++ app/src/main/java/za/co/social_engineer/www/socialengineer/api/DatabaseHandler.java
@@ -210,12 +210,8 @@ public class DatabaseHandler extends SQLiteOpenHelper {
             if ((questionSet != 100) && (questionSet != 200)) {
                 String nextQuestionQuery;
 
-                // Circular dependency - Will be removed in next version of SEADM, so hard-coded for now
-                if ((id == 3) && (state == 2) && (match == 3)) {
-                    id = 2;
-                    nextQuestionQuery = "SELECT * FROM " + TABLE_QUESTIONS + " WHERE " + KEY_ID +
-                            " = " + id;
-                } else if (questionSet == state) { // If state doesn't change return next question in current state
+                // If state doesn't change return next question in current state
+                if (questionSet == state) {
                     id++;
                     nextQuestionQuery = "SELECT * FROM " + TABLE_QUESTIONS + " WHERE " + KEY_ID +
                             " = " + id;
